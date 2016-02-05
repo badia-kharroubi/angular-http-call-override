d718066e614cce3e2c9d875de7cc4d589fe4c7d6
Badia Kharroubi <badia.kharroubi@gmail.com>
2016-02-06 00:40:05 +0100

    Final IHM

---
+++ src/app/components/github-contributor/githubContributor.service.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('oracall')
+    .factory('githubContributor', githubContributor);
+
+  /** @ngInject */
+  function githubContributor($log, $http) {
+    var apiHost = 'https://api.github.com/repos/badia-kharroubi/oracall';
+
+    var service = {
+      apiHost: apiHost,
+      getContributors: getContributors
+    };
+
+    return service;
+
+    function getContributors(limit) {
+      if (!limit) {
+        limit = 10;
+      }
+
+      return $http.get(apiHost + '/contributors?per_page=' + limit)
+        .then(getContributorsComplete)
+        .catch(getContributorsFailed);
+
+      function getContributorsComplete(response) {
+        return response.data;
+      }
+
+      function getContributorsFailed(error) {
+        $log.error('XHR Failed for getContributors.' + angular.toJson(error.data, true));
+      }
+    }
+  }
+})();
